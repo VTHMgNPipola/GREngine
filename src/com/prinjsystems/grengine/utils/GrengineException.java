220fa7fe11b7fa35b85c723b647e93a1fe478b37
VTHMgNPipola <web.anunciante@gmail.com>
2017-03-22 19:58:02 -0400

    Initial GrengineException exception class commit

---
+++ src/com/prinjsystems/grengine/utils/GrengineException.java
@@ -0,0 +1,9 @@
+package com.prinjsystems.grengine.utils;
+
+public class GrengineException extends Exception {
+	public GrengineException(String exception) {
+		super(exception);
+	}
+
+	private static final long serialVersionUID = 1L;
+}
