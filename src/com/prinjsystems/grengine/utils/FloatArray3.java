d26f67f7f33240ee0ce61d27791551c59ba1c2c3
VTHMgNPipola <web.anunciante@gmail.com>
2017-03-30 07:23:54 -0400

    Initial commit of FloatArray3

---
+++ src/com/prinjsystems/grengine/utils/FloatArray3.java
@@ -0,0 +1,32 @@
+package com.prinjsystems.grengine.utils;
+
+public class FloatArray3 {
+	
+	private float a;
+	private float b;
+	private float c;
+	
+	public FloatArray3(float a, float b, float c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+	
+	public float getA() {
+		return a;
+	}
+	
+	public float getB() {
+		return b;
+	}
+	
+	public float getC() {
+		return c;
+	}
+	
+	public void setArray(float a, float b, float c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+}
