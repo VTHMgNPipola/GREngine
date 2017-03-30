16feba4e507bcfa4a55684f8a214a3be69e1860a
VTHMgNPipola <web.anunciante@gmail.com>
2017-03-30 07:24:42 -0400

    Create DoubleArray3.java

---
+++ src/com/prinjsystems/grengine/utils/DoubleArray3.java
@@ -0,0 +1,32 @@
+package com.prinjsystems.grengine.utils;
+
+public class DoubleArray3 {
+	
+	private double a;
+	private double b;
+	private double c;
+	
+	public DoubleArray3(double a, double b, double c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+	
+	public double getA() {
+		return a;
+	}
+	
+	public double getB() {
+		return b;
+	}
+	
+	public double getC() {
+		return c;
+	}
+	
+	public void setArray(double a, double b, double c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+}
