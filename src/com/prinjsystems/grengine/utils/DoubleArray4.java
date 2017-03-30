121404405567ca85b5f4e866d7ad4c5207120406
VTHMgNPipola <web.anunciante@gmail.com>
2017-03-30 07:25:16 -0400

    Create DoubleArray4.java

---
+++ src/com/prinjsystems/grengine/utils/DoubleArray4.java
@@ -0,0 +1,39 @@
+package com.prinjsystems.grengine.utils;
+
+public class DoubleArray4 {
+	
+	private double a;
+	private double b;
+	private double c;
+	private double d;
+	
+	public DoubleArray4(double a, double b, double c, double d) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		this.d = d;
+	}
+	
+	public double getA() {
+		return a;
+	}
+	
+	public double getB() {
+		return b;
+	}
+	
+	public double getC() {
+		return c;
+	}
+	
+	public double getD() {
+		return d;
+	}
+	
+	public void setArray(double a, double b, double c, double d) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		this.d = d;
+	}
+}
