27aad2535580c830560ddd00ccd5ac381e211319
VTHMgNPipola <web.anunciante@gmail.com>
2017-03-30 07:24:18 -0400

    Create FloatArray4.java

---
+++ src/com/prinjsystems/grengine/utils/FloatArray4.java
@@ -0,0 +1,39 @@
+package com.prinjsystems.grengine.utils;
+
+public class FloatArray4 {
+	
+	private float a;
+	private float b;
+	private float c;
+	private float d;
+	
+	public FloatArray4(float a, float b, float c, float d) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		this.d = d;
+	}
+	
+	public float getA() {
+		return a;
+	}
+	
+	public float getB() {
+		return b;
+	}
+	
+	public float getC() {
+		return c;
+	}
+	
+	public float getD() {
+		return d;
+	}
+	
+	public void setArray(float a, float b, float c, float d) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		this.d = d;
+	}
+}
